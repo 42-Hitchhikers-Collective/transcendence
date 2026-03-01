ef0d9a6588c74c940ef9ed7a937d278b1f9bf554
JSlusark <slusark.jessica@gmail.com>
2026-03-01 19:44:06 +0100

    build: integrate Prettier formatting script in  and update ESLint configuration

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 /* 
   Switched to REACT-SWC: it builds faster and good for game development.
   If you want to switch back to the default babel plugin:
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite'
   The only reason to switch to the babel one is id we need babel specific feats like React Compiler or
   other specific plugins that most certainly won't be needed in our case.
 */
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 
@@ -15,9 +15,10 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     server: {
-      hmr: mode === 'development' // sets Vite's HMR to default or 443 depending where it is being run
-        ? true  // if true, it means we are running the frontend locally, this enables HMR when testing frontedn via "npm run dev"
-        : { protocol: 'wss', clientPort: 443 } // used for https for docker deployment - left as previously set up from @Welf
-    }
-  }
-})
+      hmr:
+        mode === "development" // sets Vite's HMR to default or 443 depending where it is being run
+          ? true // if true, it means we are running the frontend locally, this enables HMR when testing frontedn via "npm run dev"
+          : { protocol: "wss", clientPort: 443 }, // used for https for docker deployment - left as previously set up from @Welf
+    },
+  };
+});
