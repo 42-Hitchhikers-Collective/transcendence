2da77e3306121218d18e68c39b6408cac53eb6bd
JSlusark <slusark.jessica@gmail.com>
2026-03-02 00:10:50 +0100

    feat: implement login and signup pages components

--- apps/web/src/app/pages/login/LoginPage.tsx
+++ apps/web/src/app/pages/login/LoginPage.tsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router";
+import { GalleryVerticalEnd } from "lucide-react";
+import { LoginForm } from "@/shared/components/login-form";
 
-function LoginPage() {
+export default function LoginPage() {
   return (
-    <>
-      <p>Login page Loaded 🪵</p>
-      <Link to="/profile">Login</Link>
-      <p>
-        Dont have an account? <Link to="/signup">Sign up</Link>
-      </p>
-    </>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          Acme Inc.
+        </a>
+        <LoginForm />
+      </div>
+    </div>
   );
 }
-
-export default LoginPage;
