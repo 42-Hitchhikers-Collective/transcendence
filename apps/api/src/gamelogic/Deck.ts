221f53206d75f5079568b8b74e7f0df50ef587f1
gabrielrial <gabrielrialpicco@gmail.com>
2026-05-26 13:52:31 +0200

    fix: shuffle with phaser removed

--- apps/api/src/gamelogic/Deck.ts
+++ apps/api/src/gamelogic/Deck.ts
@@ -11,18 +11,16 @@ export class Deck {
     let id = 0;
 
     const colors = ["red", "blue", "green", "yellow"] as const;
-    const numbers = [0,1,2,3,4,5,6,7,8,9] as const;
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9] as const;
+    const specials = ["skip", "reverse", "2plus"] as const;
 
     for (const color of colors) {
-
       this.cards.push(new Card(id++, color, 0));
 
       for (const value of numbers) {
         this.cards.push(new Card(id++, color, value));
         this.cards.push(new Card(id++, color, value));
       }
-
-      const specials = ["skip", "reverse", "2plus"] as const;
 
       for (const value of specials) {
         this.cards.push(new Card(id++, color, value));
@@ -35,11 +33,16 @@ export class Deck {
       this.cards.push(new Card(id++, "wild", "4plus"));
     }
 
-    Phaser.Utils.Array.Shuffle(this.cards);
+    this.shuffle(this.cards);
   }
 
-  get_card()
-  {
+  shuffle = (array) => {
+    array.sort(() => Math.random() - 0.5);
+  }
+
+  get_card() {
     return (this.cards.pop())
   }
 }
+
+
